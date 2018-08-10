581741ff440cd6710e316773369576af6a4a6930
thegi01 <deokim77@gmail.com>
2018-08-10 17:50:35 +0900

    Add files via upload

---
+++ json/todolist.js
@@ -0,0 +1,24 @@
+
+let data = [
+    {        
+        date: '2018-08-10',
+        list: [
+            'item 1',
+            'item 2',
+            'item 3',
+            'item 4',
+            'item 5'
+        ]
+    },
+    {        
+        date: '2018-08-09',
+        list: [
+            'item 1',
+            'item 2',
+            'item 3',
+            'item 4',
+            'item 5'
+        ]
+    }
+]
+
